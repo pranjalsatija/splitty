65f89514498acb6d1613ddfc84082f93c6b89718
Pranjal Satija <me@pranj.co>
2018-08-25 15:12:55 -0500

    remove Core Data

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -12,28 +12,4 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "splitty")
-        container.loadPersistentStores(completionHandler: {(_, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-
-        return container
-    }()
-
-    func saveContext() {
-        let context = persistentContainer.viewContext
-        
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
 }
